271aa6162135fcb951fbe10887a9568c60b7dff1
DeanHdz <dean2103@gmail.com>
2024-08-19 02:09:52 -0600

    Home completado en conjunto a responsividad

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,19 +8,15 @@ export default function About() {
         <section id="About">
             <h1>Acerca de nosotros</h1>
             <div>
-                <h1>
-                    Little Lemon
-                </h1>
-                <h2>
-                    Chicago
-                </h2>
-                <p>
-                    Little lemon es un restaurante de comida italiana con mas de 10 años de experiencia en la ciudad de Chicago. Nuestro objetivo es brindar a nuestros clientes la mejor experiencia culinaria posible.
-                </p>
-            </div>
-            <div>
-                <img src={img1}></img>
-                <img src={img2}></img>
+                <div id="About-text">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>Little Lemon es un restaurante de comida italiana con más de 10 años de experiencia, fundado en la ciudad de Chicago. Desde nuestros inicios, nos hemos comprometido a ofrecer platos auténticos y de alta calidad, elaborados con ingredientes frescos y tradicionales. Nuestro objetivo es brindar a nuestros clientes la mejor experiencia culinaria posible, creando un ambiente acogedor y un servicio excepcional que haga de cada visita una ocasión memorable. Nos enorgullece ser un referente en la comunidad, donde la pasión por la buena comida se refleja en cada uno de nuestros platos.</p>
+                </div>
+                <div id="About-gallery">
+                    <img src={img1} className="about-1"></img>
+                    <img src={img2} className="about-2"></img>
+                </div>
             </div>
         </section>
     );
